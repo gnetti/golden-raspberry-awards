4a31507603df468d17b05de897efb24a642cefc6
Luiz Generoso <luiz.generoso.sislva@hotmail.com>
2025-11-11 14:36:58 -0300

    fix: update JavadocController

--- src/main/java/golden/raspberry/awards/adapter/driving/rest/controller/JavadocController.java
+++ src/main/java/golden/raspberry/awards/adapter/driving/rest/controller/JavadocController.java
@@ -1,8 +1,11 @@
 package golden.raspberry.awards.adapter.driving.rest.controller;
 
 import golden.raspberry.awards.adapter.driving.rest.dto.ApiErrorDTO;
+import golden.raspberry.awards.adapter.driving.rest.controller.constants.JavadocControllerConstants;
+import golden.raspberry.awards.adapter.driving.rest.controller.constants.ApiIllustrationSetConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -28,23 +31,29 @@ import java.util.Objects;
 public class JavadocController {
 
     @Operation(
-            summary = "Redirect to JavaDoc documentation",
-            description = "Redirects to the JavaDoc index page at /docs/index.html"
+            summary = JavadocControllerConstants.OPERATION_SUMMARY_REDIRECT_TO_JAVADOC,
+            description = JavadocControllerConstants.OPERATION_DESCRIPTION_REDIRECT_TO_JAVADOC
     )
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "302",
-                    description = "Redirect to JavaDoc index page"
+                    responseCode = JavadocControllerConstants.HTTP_STATUS_CODE_FOUND_STRING,
+                    description = JavadocControllerConstants.API_RESPONSE_DESCRIPTION_REDIRECT
             ),
             @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal Server Error",
-                    content = @Content(schema = @Schema(implementation = ApiErrorDTO.class))
+                    responseCode = JavadocControllerConstants.HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR_STRING,
+                    description = JavadocControllerConstants.API_RESPONSE_DESCRIPTION_INTERNAL_SERVER_ERROR,
+                    content = @Content(
+                            schema = @Schema(implementation = ApiErrorDTO.class),
+                            examples = @ExampleObject(
+                                    name = "Internal Server Error",
+                                    value = ApiIllustrationSetConstants.ILLUSTRATION_SET_ERROR_INTERNAL_SERVER
+                            )
+                    )
             )
     })
     @GetMapping
     public void redirect(HttpServletResponse response) throws IOException {
-        Objects.requireNonNull(response, "HttpServletResponse cannot be null");
+        Objects.requireNonNull(response, JavadocControllerConstants.ERROR_MESSAGE_HTTP_SERVLET_RESPONSE_CANNOT_BE_NULL);
         response.sendRedirect("/docs/index.html");
     }
 
